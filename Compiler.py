9fb048827fa7c45d85e5227082c23fd2efc3defd
Ondřej Bína <xbinao@seznam.cz>
2024-04-04 21:11:31 +0200

    Compiler - py, pyc

--- Compiler.py
+++ Compiler.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+import py_compile
+import uuid
 from ConsoleAppTemplate import *
 
+"""
+Version: 2024.04.04
+"""
 
 class Module:
 	__py_header: str = '#!/usr/bin/env python3'
@@ -72,12 +77,14 @@ class Module:
 		self.imports = dict(sorted(self.imports.items()))
 
 class Compiler(Core):
+	__log_format: str = '{:.<20}..'
 	def __init__(self):
 		super().__init__(
 			'Compiler',
 			Menu(
 				{
-					'C': MenuItem('Compile', self.compile),
+					'C': MenuItem('Compile to .py', self.compile_py_prompt),
+					'B': MenuItem('Compile to .pyc (byte-code)', self.compile_pyc_prompt),
 					'q': MenuItem('{{self.config.locale.QUIT}}', exit),
 				},
 				self
@@ -85,9 +92,43 @@ class Compiler(Core):
 			Config()
 		)
 		self.core_files: list[str] = ['Compiler.py', 'Locales.py', 'ConsoleAppTemplate.py']
-		self.result_suffix = '_StandAlone.py'
+		self.result_suffix = '_StandAlone'
 
-	def compile(self) -> None:
+
+	def compile_py_prompt(self) -> None:
+		app_file = self.get_app_file_prompt()
+		if not app_file:
+			return
+
+		app_dst_py = os.path.splitext(app_file)[0] + self.result_suffix + '.py'
+		if os.path.exists(app_dst_py) and not self.input_bool('\nFile \'' + app_dst_py + '`\' already exists. Would you like to overwrite it?'):
+			return
+
+		print()
+		self.compile_py(app_file, app_dst_py)
+		self.input(5*'=' + ' ALL DONE ' + 5*'=')
+
+	def compile_pyc_prompt(self) -> None:
+		app_file = self.get_app_file_prompt()
+		if not app_file:
+			return
+
+		app_tmp_py = app_file
+		while os.path.exists(app_tmp_py):
+			app_tmp_py = 'tmp_' + uuid.uuid4().hex + '.py'
+
+		app_dst_pyc = os.path.splitext(app_file)[0] + self.result_suffix + '.pyc'
+		if os.path.exists(app_dst_pyc) and not self.input_bool('\nFile \'' + app_dst_pyc + '`\' already exists. Would you like to overwrite it?'):
+			return
+
+		print()
+		self.compile_py(app_file, app_tmp_py)
+		self.compile_pyc(app_tmp_py, app_dst_pyc)
+		os.remove(app_tmp_py)
+		self.input(5*'=' + ' ALL DONE ' + 5*'=')
+
+
+	def compile_py(self, app_src: str, app_dst: str) -> None:
 		def make_code_block(name: str, code: str) -> str:
 			separator_format: str = '#{:=^78}#\n'
 			code_block = separator_format.format(' BEGIN: ' + name.upper() + ' ')
@@ -95,32 +136,20 @@ class Compiler(Core):
 			code_block += separator_format.format(' END: ' + name.upper() + ' ')
 			return code_block
 
-		log_format = '{:.<20}..'
-
-		app_file = self.get_app_file_prompt()
-		if not app_file:
-			return
-
-		app_file_stand_alone = os.path.splitext(app_file)[0] + self.result_suffix
-		if os.path.exists(app_file_stand_alone) and not self.input_bool('\nFile \'' + app_file_stand_alone + '`\' already exists. Would you like to overwrite it?'):
-			return
-
-		print()
-
-		print(log_format.format('Loading '), end='', flush=True)
+		print(self.__log_format.format('Loading '), end='', flush=True)
 		# module - locale
 		module_locales: Module = Module(self.core_files[1])
 		# module - core
 		module_core: Module = Module(self.core_files[2])
 		module_core.pop_import(os.path.splitext(self.core_files[1])[0])
 		# module - app
-		module_app: Module = Module(app_file)
+		module_app: Module = Module(app_src)
 		module_app.pop_import(os.path.splitext(self.core_files[2])[0])
 		print(' [ DONE ]')
 
-		print(log_format.format('Merging '), end='', flush=True)
+		print(self.__log_format.format('Merging '), end='', flush=True)
 		# final module
-		final_module: Module = Module(app_file_stand_alone, False)
+		final_module: Module = Module(app_dst, False)
 
 		final_module.add_imports(module_locales.imports)
 		final_module.add_imports(module_core.imports)
@@ -132,16 +161,18 @@ class Compiler(Core):
 		final_module.code += '\n' + make_code_block('App', module_app.code)
 		print(' [ DONE ]')
 
-		print(log_format.format('Saving '), end='', flush=True)
+		print(self.__log_format.format('Saving '), end='', flush=True)
 		final_module.save()
 		print(' [ DONE ]')
 
-		self.input()
-
+	def compile_pyc(self, app_src: str, app_dst: str) -> None:
+		print(self.__log_format.format('Compiling '), end='', flush=True)
+		py_compile.compile(app_src, app_dst)
+		print(' [ DONE ]')
 
 	def get_app_file_prompt(self) -> str|None:
 		files: list[str] = list(filter(
-			lambda x: x.endswith('.py') and os.path.isfile(x) and x not in self.core_files and not x.endswith(self.result_suffix),
+			lambda x: x.endswith('.py') and os.path.isfile(x) and x not in self.core_files and not x.endswith(self.result_suffix + '.py'),
 			os.listdir()
 		))
 		files.sort()
